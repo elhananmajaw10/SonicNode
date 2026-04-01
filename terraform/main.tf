f82e035a3a42533ac9db1a3fe752bd1b1d5bc151
elhanan <elhananmajaw10@gmail.com>
2026-04-01 08:27:29 +0530

    Added main.tf so Git uploads the terraform folder

---
+++ terraform/main.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+}
+
+resource "aws_instance" "sonicnode_server" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "SonicNode-Production-Server"
+  }
+}
